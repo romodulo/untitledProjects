44960087f1fae576d63a3d217d57b4b093003a69
romel-romodulo <57033860+romodulo@users.noreply.github.com>
2025-01-29 13:52:49 +1000

    Last commit of < 1-28> time:  <11:59>: created this commit because I forgot to commit last night.

---
+++ flask_app_tutorial/flask_app_crud.py
@@ -0,0 +1,23 @@
+# create a virtual environment
+#     python3 -m venv .myvenv
+#     activate .myvenv: source .myvenv/bin/activate
+# create some files, and directories for the application
+#     each template is under a specific directory tree...
+# create an application factory
+#     function definition name: create_app()
+#     config = None as the parameter
+
+
+# notes**at some point, I will need to connect the database
+# connect the database
+#     import sqlite3
+#     copied-notes: from datetime import datetime
+
+# create a blueprint
+
+#To-do List Idea
+#Business Plan Tracker
+#   What do I want to do? 
+
+def create_app(test_config=None):
+    pass
